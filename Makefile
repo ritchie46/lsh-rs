65bda0841ac1cedd9dbf435239fd522449822b8f
ritchie46 <ritchie46@gmail.com>
2020-03-18 13:48:55 +0100

    fix stats bugs and add tests

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ bench:
 	@cd lsh-rs/bench && cargo +nightly bench
 
 test:
-	@cd lsh-rs && cargo test --lib
+	@cd lsh-rs && cargo test --features stats --lib
 
 python-lib:
 	@cd lsh-py && cargo +nightly build --release && cd .. && cp target/release/liblshpy.so lshpy.so
--- lsh-rs/src/stats.rs
+++ lsh-rs/src/stats.rs
@@ -22,8 +22,29 @@ pub fn l2_ph(r: f64, c: f64) -> f64 {
 ///
 /// # Arguments
 /// * `delta` - Prob. not returned NN.
-/// * `p_1` - P1 in literature.
+/// * `p1` - P1 in literature.
 /// * `k` - Number of hash projections.
-pub fn estimate_l(delta: f64, p_1: f64, k: usize) -> usize {
-    (delta.ln() / (1 - p_1.powf(k as f64).ln())) as usize
+pub fn estimate_l(delta: f64, p1: f64, k: usize) -> usize {
+    (delta.ln() / (1. - p1.powf(k as f64)).ln()).round() as usize
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_l2_ph() {
+        // tested w/ numpy
+        let r = 2.0;
+        let c = 1.0;
+        assert_eq!(0.609548422215397, l2_ph(r, c) as f32);
+    }
+
+    #[test]
+    fn test_estimate_l() {
+        let delta = 0.2;
+        let p1 = 0.6;
+        let k = 5;
+        assert_eq!(20, estimate_l(delta, p1, k));
+    }
 }
